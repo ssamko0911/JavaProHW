8ea6a0edcb9b05ffc9f0ba636b858c9fd137145c
samko.s <ssamko091184@gmail.com>
2021-12-15 18:58:52 +0200

    Task 3 - done.

---
+++ src/homeworktocheck/hw002pro/task003/MinLinkedList.java
@@ -0,0 +1,36 @@
+package homeworktocheck.hw002pro.task003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class MinLinkedList {
+
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.println("Введите количество элементов связанного списка: ");
+        System.out.printf("Минимальное значение равно: %d.", getMinimum(getIntegerList(Integer.parseInt(in.readLine()), list)));
+    }
+
+    public static LinkedList<Integer> getIntegerList(int numberOfElements, LinkedList<Integer> someList) throws IOException {
+        for (int i = 0; i < numberOfElements; i++) {
+            System.out.printf("Введите %dе значение коллекции:\n", i + 1);
+            someList.add(Integer.parseInt(in.readLine()));
+        }
+        return someList;
+    }
+
+    public static Integer getMinimum(LinkedList<Integer> someList) {
+        int minimum = someList.get(0);
+        for (Integer integer : someList) {
+            if (integer < minimum) {
+                minimum = integer;
+            }
+        }
+        return minimum;
+    }
+}
