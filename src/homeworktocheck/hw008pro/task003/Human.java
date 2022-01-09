9d7457f9f0cf3df2915775fa5ddc1f1191ca054a
samko.s <ssamko091184@gmail.com>
2022-01-09 14:35:08 +0200

    Task 3 done

---
+++ src/homeworktocheck/hw008pro/task003/Human.java
@@ -0,0 +1,8 @@
+package homeworktocheck.hw008pro.task003;
+
+@MyInhatitantAnnotation (info = "Hi all! This is the most usefull anno I`ve ever seen!")
+@WithOutInharitance
+public class Human {
+    String name;
+    int age;
+}
